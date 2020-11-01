5735b2f2346d59094eacd10cc94264f05580617a
Michael Shuff <mjshuff23@gmail.com>
2020-10-31 21:16:50 -0400

    too much

--- app.js
+++ app.js
@@ -6,17 +6,78 @@ const usersRouter = require("./routes/users");
 const serversRouter = require("./routes/servers");
 const channelsRouter = require("./routes/channels");
 const { environment } = require("./config");
+const { Channel } = require('./db/models');
+const { addMessageToChannel } = require("./utils");
 
+// Setup socket.io to work with express
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
+// Setup middleware
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 
+// Setup api routes
 app.use("/users", usersRouter);
 app.use("/servers", serversRouter);
 app.use('/channels', channelsRouter);
 
+// Setup socket.io server to listen for connections
+io.on('connection', async (socket) => {
+  console.log(`${socket.id} -- Connected`);
+
+  // When joined, retrieve channel from DB and join socket to channel
+  // Makes it so messages to this channel flow back to user's connection
+  socket.on('join', async (channel) => {
+    console.log(channel);
+    let channelObject = await Channel.findByPk(channel.id);
+    if (channelObject) {
+      socket.join(channelObject.id, async () => {
+        console.log(`${socket.id} has joined channel ${channelObject.title}`);
+      });
+    }
+  });
+
+  // When leaving a channel, get channel, and inform socket we have left
+  socket.on('leave', async (channel) => {
+    console.log(channel);
+    let channelObject = await Channel.findByPk(channel.id);
+    if (channelObject) {
+      socket.leave(channelObject.id, async () => {
+        console.log(`${socket.id} has left ${channelObject.title}`);
+      });
+    }
+  });
+
+  // When socket disconnects, log to console
+  socket.on('disconnect', () => {
+    console.log(`${socket.id} has disconnected.`);
+  });
+
+  // Get channels from DB
+  const channels = await Channel.findAll();
+
+  // Loop through all the channels and add listeners for messages to all channels
+  for (let channel of channels) {
+    console.log(`Listening for messages from ${channel.title}`);
+    // Steps for getting a message for channel
+    //  1. Log message
+    //  2. Add message to DB with addMessageToChannel function
+    //  3. Emit message to channel and back to sender
+    //
+    //  Note: socket.to only sends messages to other sockets joined to the room
+    //        this is why we have to send the message back to original socket
+
+    socket.on(channel.id, async ({ message, userId }) => {
+      console.log(`${channel.title} -- ${userId}: ${message}`);
+      const newMessage = await addMessageToChannel(userId, channel.id, message);
+      socket.to(channel.id).emit(channel.id, newMessage);
+      socket.emit(channel.id, newMessage);
+    });
+  }
+});
 
 // Catch unhandled requests and forward to error handler.
 app.use((req, res, next) => {
@@ -50,4 +111,4 @@ app.use((err, req, res, next) => {
   });
 });
 
-module.exports = app;
+module.exports = http;
--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const { User, Channel, Channel_Message } = require('./db/models');
 
 const asyncHandler = (handler) => (req, res, next) =>
   handler(req, res, next).catch(next);
@@ -18,4 +19,28 @@ const handleValidationErrors = (req, res, next) => {
   next();
 };
 
-module.exports = { asyncHandler, handleValidationErrors };
+const addMessageToChannel = async (userId, channelId, messageContent) => {
+  console.log('Got here');
+  // Attempt to find Channel and create Message to database
+  try {
+    // Find user by username
+    const user = await User.findByPk(userId);
+    const channel = await Channel.findByPk(channelId);
+    const message = await Channel_Message.create({
+      body: messageContent,
+      userId,
+      channelId,
+    });
+    // await message.save();
+    // Return message and channel objects
+    return {
+      message,
+      channel,
+      user,
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports = { asyncHandler, handleValidationErrors, addMessageToChannel };
